de857f8a46ff5611a0613c10e75c02a7274b93c7
omarsmarinhos <omarmarinosaguilar@gmail.com>
2023-01-06 13:07:53 -0500

    user service implemented

--- src/main/java/com/omarinhos/servlet/services/UsuarioService.java
+++ src/main/java/com/omarinhos/servlet/services/UsuarioService.java
@@ -2,9 +2,14 @@ package com.omarinhos.servlet.services;
 
 import com.omarinhos.servlet.models.Usuario;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioService {
 
     Optional<Usuario> login(String username, String password);
+    Optional<Usuario> porId(Long id);
+    void guardar(Usuario usuario);
+    void eliminar(Long id);
+    List<Usuario> listar();
 }
--- src/main/java/com/omarinhos/servlet/services/UsuarioServiceImpl.java
+++ src/main/java/com/omarinhos/servlet/services/UsuarioServiceImpl.java
@@ -6,6 +6,7 @@ import com.omarinhos.servlet.repositories.UsuarioRepositoryImpl;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 public class UsuarioServiceImpl implements UsuarioService{
@@ -21,6 +22,42 @@ public class UsuarioServiceImpl implements UsuarioService{
         try {
             return Optional.ofNullable(repository.porUsername(username))
                     .filter(usuario -> usuario.getPassword().equals(password));
+        } catch (SQLException e) {
+            throw new ServiceJdbcException(e.getMessage(), e.getCause());
+        }
+    }
+
+    @Override
+    public Optional<Usuario> porId(Long id) {
+        try {
+            return Optional.ofNullable(repository.porId(id));
+        } catch (SQLException e) {
+            throw new ServiceJdbcException(e.getMessage(), e.getCause());
+        }
+    }
+
+    @Override
+    public void guardar(Usuario usuario) {
+        try {
+            repository.guardar(usuario);
+        } catch (SQLException e) {
+            throw new ServiceJdbcException(e.getMessage(), e.getCause());
+        }
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        try {
+            repository.eliminar(id);
+        } catch (SQLException e) {
+            throw new ServiceJdbcException(e.getMessage(), e.getCause());
+        }
+    }
+
+    @Override
+    public List<Usuario> listar() {
+        try {
+            return repository.listar();
         } catch (SQLException e) {
             throw new ServiceJdbcException(e.getMessage(), e.getCause());
         }
